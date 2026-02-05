531dab68c4b7efe2219f1bf3e993705a6dc4344e
Santiago Paak <santiago.paak@gmail.com>
2026-02-05 18:02:38 -0300

    feat: New map + random holes

--- Scenes/StatusController.gd
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f3d3063d0e8ebade0c985cf1ca5063fe73aab4544e595a6389493f32be8025ee
-size 770
---
+++ Scripts/holes.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:685e45df1a142d056b48aa6880d27ef235d53d26383bde164fa7abad38f8f49f
+size 823
