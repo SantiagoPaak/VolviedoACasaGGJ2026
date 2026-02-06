2a49f44a0e647e607cbc3e39b18d0fcd1936a231
Santiago Paak <santiago.paak@gmail.com>
2026-02-05 21:30:26 -0300

    feat: Added Sun

--- Scripts/holes.gd
+++ Scripts/holes.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:685e45df1a142d056b48aa6880d27ef235d53d26383bde164fa7abad38f8f49f
-size 823
+oid sha256:9f4285f03846704da1455bf50c84448d94e04bd55603fa31dff3a351a830f0ba
+size 822
